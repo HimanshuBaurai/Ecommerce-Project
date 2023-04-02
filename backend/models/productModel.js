087c548b6156030a85b56bc94e88c92d5a39681c
Himanshu <himanshucosmicera@gmail.com>
2023-04-02 20:18:13 +0530

    Added routes for Products and also made product model schema

---
+++ backend/models/productModel.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter product name'],
+        trim: true
+    },
+    description: {
+        type: String,
+        required: [true, 'Please enter product description']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Please enter product price'],
+        maxLength: [8, 'Price cannot exceed 8 characters']
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    images: [
+        //can have multiple images, thus array of images
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    category: {
+        type: String,
+        required: [true, 'Please Enter category for this product'],
+    },
+    Stock: {
+        type: Number,
+        required: [true, 'Please Enter Stock for this product'],
+        maxLength: [4, 'Stock cannot exceed 4 characters'],
+        default: 1
+    },
+    numOfReviews: {
+        type: Number,
+        default: 0
+    },
+    reviews: [
+        {
+            name: {
+                type: String,
+                required: true,
+            },
+            rating: {
+                type: Number,
+                required: true,
+            },
+            comment: {
+                type: String,
+                required: true,
+            }
+        }
+    ],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Product', productSchema);
