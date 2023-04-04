9392b0da6f3643ec28f7e9944ce7059a40069509
Himanshu <himanshucosmicera@gmail.com>
2023-04-04 22:07:23 +0530

    made error handler for productController, but its not working as tested on postman, would be rectifying later on, right now unable to debug

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const errorMiddleware = require('./middleware/error');
+
 app.use(express.json());
 
 //Routes import
@@ -8,5 +10,7 @@ const product = require('./routes/productRoute');
 
 app.use("/api/v1", product);
 
+//middleWare to handle errors
+app.use(errorMiddleware);
 
 module.exports = app;
