9ad066dcae843f2fd9f0d135a1ba90f69632575d
Himanshu <himanshucosmicera@gmail.com>
2023-04-04 22:52:03 +0530

    handled unCaught Exceptions, unhandled Promise Rejection and castError

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -4,6 +4,12 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server Error";
 
+    //wrong Mongodb id error-->know as castError
+    if (err.name === "CastError") {
+        const message = `Resource not found. Invalid: ${err.path}`;
+        err = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message,
--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,32 @@ const app = require('./app');
 const dotenv = require('dotenv');
 const connnectDatabase = require('./config/database');
 
+//Handling uncaught Exception, like if at any place we have console.log(youtube) and youtube is not defined, so this is know as uncaught exception and we exit process
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to uncaught Exception`);
+    //exit process
+    process.exit(1);
+});
+
 //config
 dotenv.config({ path: 'backend/config/config.env' });
+
 //connect to database
 connnectDatabase();
 
 
-
-
-
-
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server is running on https://localhost:${process.env.PORT}`);
+});
+
+
+//unhandled promise rejection--> when mongo url is wrong/invalid
+process.on("unhandledRejection", err => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to unhandled Promise Rejection`);
+    //and when we encounter this error we want to close the server and exit the process
+    server.close(() => {
+        process.exit(1);
+    });
 });
